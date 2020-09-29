9263422b8e88402de6414dc1922c2ac326fc5b46
claudmiine <wojcieska.klaudia@gmail.com>
2020-09-29 19:10:18 +0100

    p2.py

---
+++ P2.py
@@ -0,0 +1,12 @@
+"""P2 	
+Write a Python program that allows the user to enter any integer 
+value, and displays the value of two raised to that power.  
+Your program should function as shown below. """
+
+def python_power():
+    num = int(input("Give a number: "))
+    answer = 2 ** num
+    print(answer)
+
+python_power()
+python_power()
---
+++ hello.py
@@ -0,0 +1,10 @@
+"""Following the example in class, create a file Part2.py 
+ which reads your name from the keyboard and then prints a personalised greeting."""
+
+def personal_details():
+    name = input("Input your name: ")
+    # f strings
+    print(f"Hello {name}")
+    
+
+personal_details()
